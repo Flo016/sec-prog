1b20ae8800a47ad2d69511582118b5d68e5fc7f7
Flo016 <83062949+Flo016@users.noreply.github.com>
2021-12-08 15:13:11 +0100

    Create client.py

---
+++ client.py
@@ -0,0 +1,19 @@
+import socket
+
+class client:
+
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+
+    clientSocket.connect(("127.0.0.1", 600))   # TODO use secure socket
+
+
+    while True:
+
+        userMessage = input("Please write your message :)  :") # TODO (Maybe) Inputvalidation
+        print(userMessage)
+        userMessage = userMessage.encode()  #MAC
+
+        print("AÖKDSJHLÖKSAJÖKLASJÖDLASJÖLDKLASD")
+        clientSocket.send(userMessage)
