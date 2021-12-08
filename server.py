01f990c0c9ee0921b06455f4c041a7d2683b9326
Flo <nelles-florian@web.de>
2021-12-08 15:58:30 +0100

    huhu

--- server.py
+++ server.py
@@ -16,27 +16,31 @@ class Server:
 
 
     def client_connect(self, client_Server_Socket,array):  # TODO give GO! for user Information # TODO Log in User
+        """ Wait for messages, one per loop cycle"""
         while True:
+
             data_From_Client = client_Server_Socket.recv(1024).decode()
-
-
             print(str(array[0]) +":"+str(array[1]) + " wrote: " +data_From_Client)  # TODO Store data in user   #TODO Send stored data
 
     def main_loop(self):
 
+        """ Create Server Socket and listen"""
         self.server_Socket.bind(("127.0.0.1", 600))  # TODO use secure socket
         print("Server Listening...")
         self.server_Socket.listen()
 
+        """ Handle one connection per loop cycle"""
         while True:
 
-
+            """ Wait for connections """
             (client_Connected, client_Address) = self.server_Socket.accept()
 
+            """ Create Array with User Information(IP & Port) """
             array = []
             for item in client_Connected.getpeername():
                 array.append(item)
 
+            """ Give Thread to Client """
             thread_1 = threading.Thread(target=self.client_connect, args=(client_Connected, array,))
             thread_1.start()
 
