d7e5723eed883c0ac2b17b4ccf648609de6d7cc4
Flo <nelles-florian@web.de>
2021-12-09 13:43:13 +0100

    commit

--- client.py
+++ client.py
@@ -1,17 +1,72 @@
 import socket
+# import rsa
+import secrets  # TODO maybe explain why this library?
 
 """Start Client separately from Server"""
-class client:
-    """Connect to Server Socket"""
-    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+class Client:
+
+    def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.user_login()
+        command = input("Select what you want to do:")
+
+
+    def user_login(self):
+        """Connect to Server Socket"""
+        self.client_socket.connect(("127.0.0.1", 600))  # TODO use secure socket
+
+        print("connected")
+        try:
+            """If log in data exists, try to log in with it"""
+            with open("client_login_data.txt", 'r') as user_login:
+                self.client_socket.send("no".encode())
+                lines = user_login.readlines()  # [0] = Username [1] = Password
+                self.client_socket.send(lines[0].encode())  # send username
+                self.client_socket.recv(1024)  # wait for "send password"
+                self.client_socket.send(lines[1].encode())  # send password
+                """wait for confirmation"""
+
+
+        except:
+            """If log in data doesn't exist, create a new account"""
+            self.client_socket.send("yes".encode())
+            username = input("No login data found - Please select a Username:")
+
+            with open("client_login_data.txt", 'w') as user_login:
+                user_login.write(username + '\n')
+
+                """Create a new Password"""
+                possible_symbols = "#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+                password = ''
+                for i in range(30):
+                    password = password + secrets.choice(possible_symbols)
+                user_login.write(password + '\n')
+
+                """ public_key, private_key = rsa.newkeys(512) #TODO Secure Private key in safe file
+                user_login.write(public_key+"\n")
+                user_login.write(private_key+"\n")"""
+
+            self.client_socket.send(username.encode())  # send Username
+            self.client_socket.recv(1024)  # wait for "send password"
+            self.client_socket.send(password.encode())  # send Password TODO send encrypted password
+
+            pass
 
 
 
-    clientSocket.connect(("127.0.0.1", 600))   # TODO use secure socket
+    """
+    def write_message(self):
+        #Communication with Server
+        while True:
+            #public_key, private_key = rsa.newkeys(512)
+            user_message = input("Please write your message :)  :")
+            print("original string: ", user_message)
+            user_message = rsa.encrypt(user_message.encode(), public_key)  # TODO encrypt data
+            print("encrypt string: ", user_message)
+            user_message = rsa.decrypt(user_message, private_key).decode()
+            print("decrypted string: ", user_message)
+    """
+    # def encrypt_message(self, message):
 
-    """Communication with Server"""
-    while True:
-        userMessage = input("Please write your message :)  :")
-        print(userMessage)
-        userMessage = userMessage.encode()  #TODO encrypt data
-        clientSocket.send(userMessage)
--- server.py
+++ server.py
@@ -10,43 +10,84 @@ class Server:
         self.server_Socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.main_loop()
 
-    #Haha
-
-
-
-
-    def client_connect(self, client_Server_Socket,array): #client_Server_Socket: Socket Object; array: [Client_IP; Client_Port]
+    def client_connect(self, client_server_socket,array):  # client_Server_Socket: Socket Object; array: [Client_IP; Client_Port]
         # TODO give GO! for user Information # TODO Log in User
 
-        """ Wait for messages, one per loop cycle"""
-        while True:
-
-            data_From_Client = client_Server_Socket.recv(1024).decode()
-            print(str(array[0]) +":"+str(array[1]) + " wrote: " +data_From_Client)  # TODO Store data in user   #TODO Send stored data
-            # TODO (Maybe) Inputvalidation for log in data
+        """User logs in"""
+        if self.log_in_request(client_server_socket):
+            print("stuff successful")
+            """ Wait for messages, one per loop cycle"""
+            #while True:
+                #data_from_client = client_server_socket.recv(1024).decode()
+                #print(str(array[0]) + ":" + str(
+                    #array[1]) + " wrote: " + data_from_client)  # TODO Store data in user   #TODO Send stored data
+                # TODO (Maybe) Input validation for log in data
+        else:  # Userpassword false
+            client_server_socket.close
+            return
 
     def main_loop(self):
 
         """ Create Server Socket and listen"""
-        self.server_Socket.bind(("127.0.0.1", 600))  # TODO use secure socket
+        self.server_socket.bind(("127.0.0.1", 600))  # TODO use secure socket
         print("Server Listening...")
-        self.server_Socket.listen()
+        self.server_socket.listen()
 
         """ Handle one connection per loop cycle"""
         while True:
 
             """ Wait for connections """
-            (client_Connected, client_Address) = self.server_Socket.accept()
+            (client_connected, client_Address) = self.server_socket.accept()
 
             """ Create Array with User Information(IP & Port) """
             array = []
-            for item in client_Connected.getpeername():
+            for item in client_connected.getpeername():
                 array.append(item)
 
-            """ Give Thread to Client """
-            thread_1 = threading.Thread(target=self.client_connect, args=(client_Connected, array,))
+            """ Give Thread to Client and initialize communication"""
+            thread_1 = threading.Thread(target=self.client_connect, args=(client_connected, array,))
             thread_1.start()
-
-            #self.client_connect(client_Connected)
-
             print("Accepted connection from " + str(array[0]) + ":" + str(array[1]))
+
+    def log_in_request(self, client_server_socket):
+        """register an account? - This process is automated with client, if client doesnt find local log in data
+            it creates an account, if it does, it performs a login"""
+
+        data_from_client = client_server_socket.recv(1024).decode()
+
+        """Request Username and Password  with file name after username"""
+        client_server_socket.send("Username?".encode())  # TODO Check if Username already exists; Change String - "automated process"
+        username_from_client = client_server_socket.recv(1024).decode()
+        client_server_socket.send("Password?".encode())
+        password_from_client = client_server_socket.recv(1024).decode()
+        user_file_name = "login_data_" + username_from_client  # Create User filename
+
+        # TODO Implement safe passwords with salt (maybe pepper)
+        # TODO Implement public key for RSA Encryption
+
+        while True:
+            if data_from_client == "yes":
+                #client_server_socket.send("Public key?")
+                #public_key_from_client = client_server_socket.recv(1024).decode()
+
+                """ Create new file if it doesn´t exist """
+
+                login_data = open("{name}.txt".format(name=user_file_name), "w")  # TODO Check Program privileges
+                login_data.write(password_from_client)  # TODO Implement public key for RSA Encryption
+
+                login_data.close()
+                return True
+            else:
+                try:
+                    with open("{name}.txt".format(name=user_file_name), 'r') as login_data:
+                        password = login_data.read()
+                        if password == password_from_client:
+                            client_server_socket.send("success")
+                            return True  # Log in success
+                        else:
+                            client_server_socket.send("ERROR: Wrong Password, please try again".encode())
+                            """Can only be false, if client data has been manipulated, therefore we close connection"""
+                            return False
+                except:
+                    client_server_socket.send("no login data found, creating new account".encode())
+                    data_from_client = "yes"
