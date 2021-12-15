e1c1625591dad6be58d1dd70493d78390c61859b
Flo <nelles-florian@web.de>
2021-12-15 22:17:43 +0100

    Added Username ID's for multiple instances of one username

--- client.py
+++ client.py
@@ -10,7 +10,7 @@ class Client:
     def __init__(self):
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.user_login()
-        command = input("hallo")
+        command = input("what do you want to do")
         self.write_message()
 
 
@@ -23,41 +23,53 @@ class Client:
             """If log in data exists, try to log in with it"""
             with open("client_login_data.txt", 'r') as user_login:
                 self.client_socket.send("no".encode())
-                line = user_login.read()
+                self.client_socket.recv(1024)  # each empty recv is a "wait for server"
+                line = user_login.read() # example file: Username,Password
                 lines = line.split(',')
-                self.client_socket.recv(1024).decode()
                 self.client_socket.send(lines[0].encode())  # send username
-                self.client_socket.recv(1024).decode()  # wait for "send password"
+                self.client_socket.recv(1024)
                 self.client_socket.send(lines[1].encode())  # send password
                 """wait for confirmation"""
 
         except:
-            print("exception")
             """If log in data doesn't exist, create a new account"""
             self.client_socket.send("yes".encode())
+            self.client_socket.recv(1024)
             username = input("No login data found - Please select a Username:")
+
+            """Automatically create a new Password"""
+            possible_symbols = "#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+            password = ''
+            for i in range(30):
+                password = password + secrets.choice(possible_symbols)
+
+            self.client_socket.send(username.encode())  # send Username
+            self.client_socket.recv(1024)
+            self.client_socket.send(password.encode())  # send Password TODO send encrypted password
+
+            while True:
+                approved_username = self.client_socket.recv(1024).decode()
+                print(approved_username)
+                if approved_username == "Username unavailable. Please select another Username":
+                    username = input("")
+                    self.client_socket.send(username.encode())
+                else:
+                    break
+
 
             with open("client_login_data.txt", 'w') as user_login:
 
-                """Create a new Password"""
-                possible_symbols = "#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-                password = ''
-                for i in range(30):
-                    password = password + secrets.choice(possible_symbols)
-                user_login.write(username +"," + password)
+                user_login.write(username + "," + password)
 
                 """ public_key, private_key = rsa.newkeys(512) #TODO Secure Private key in safe file
                 user_login.write(public_key+"\n")
                 user_login.write(private_key+"\n")"""
 
-            self.client_socket.send(username.encode())  # send Username
-            self.client_socket.recv(1024)  # wait for "send password"
-            self.client_socket.send(password.encode())  # send Password TODO send encrypted password
 
-            pass
 
     def write_message(self):
         self.client_socket.send("message".encode())
+        self.client_socket.recv(1024)
         user = input("Which user should receive this message?\n")
         message = input("What do you want to tell " + user + "?")
         self.client_socket.send(user.encode()) # sends message receiver
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+from random import randrange
 
 
 
@@ -13,28 +14,15 @@ class Server:
     def client_connect(self, client_server_socket,array):  # client_Server_Socket: Socket Object; array: [Client_IP; Client_Port]
         # TODO give GO! for user Information
         """User logs in"""
-        if self.log_in_request(client_server_socket):#TODO Send stored public key
+        if self.log_in_request(client_server_socket):  # TODO Send stored public key
             while True:
                 print("stuff successful")
                 command = client_server_socket.recv(1024).decode()
 
                 if command == "message":
-                    user = client_server_socket.recv(1024).decode()
-                    try:
-                        # check if file exists
-                        file = open("{name}.txt".format(name=user), "r")
-                        file.close()
-                        user_file = open("{name}.txt".format(name=user), "w")
-                        client_server_socket.send("send message".encode())
-                        message = client_server_socket.recv(1024).decode()
-                        user_file.write("")
-                        user_file.write("\n"+ message)
-                        user_file.close()
-                        client_server_socket.send("end message".encode())
-                    except:
-                        print("excption")
+                    self.user_message()
 
-                #TODO (Maybe) Input validation for log in data
+
         else:  # Userpassword false
             client_server_socket.close
             return
@@ -62,47 +50,41 @@ class Server:
             thread_1.start()
             print("Accepted connection from " + str(array[0]) + ":" + str(array[1]))
 
-    def log_in_request(self, client_server_socket):
-        """register an account? - This process is automated with client, if client doesnt find local log in data
-            it creates an account, if it does, it performs a login"""
 
-        data_from_client = client_server_socket.recv(1024).decode()
+
+    def log_in_request(self, client_server_socket):
+        # TODO MAC for yes and no
+        """register an account? - This process is automated with client, if client doesnt find local log in data
+            it creates an account(data_from_client = yes), if it does, it performs a login(data_from_client = no)"""
+
+        createAccount = client_server_socket.recv(1024).decode()
 
         """Request Username and Password  with file name after username"""
-        print("Hallo2")
-        client_server_socket.send("Username?".encode())  # TODO Check if Username already exists; Change String - "automated process"
-        print("Hallo1")
+        client_server_socket.send("Username?".encode())
         username_from_client = client_server_socket.recv(1024).decode()
 
         client_server_socket.send("Password?".encode())
-        print(username_from_client)
         password_from_client = client_server_socket.recv(1024).decode()
-        user_file_name = "login_data_" + username_from_client + ".txt"
-
-
-        # TODO Implement safe passwords with salt (maybe pepper)
-        # TODO Implement public key for RSA Encryption
 
         while True:
-            if data_from_client == "yes":
-                #client_server_socket.send("Public key?")
-                #public_key_from_client = client_server_socket.recv(1024).decode()
-
-                """ Create new file if it doesn´t exist """
-                print(user_file_name)
-                print(user_file_name)
-                login_data = open('{}.txt'.format(user_file_name), "w")  # TODO Check Program privileges
-                login_data.write(password_from_client)  # TODO Implement public key for RSA Encryption
-
-
-                login_data.close()
+            if createAccount == "yes":
+                self.create_account(client_server_socket, username_from_client, password_from_client)
                 return True
+
             else:
                 try:
-                    login_data = open('{}.txt'.format(user_file_name), 'r')
-                    password = login_data.readlines()
+                    # file consist of: ("Password, Salt, (Message, Sender)*")
+                    login_data = open('{}.txt'.format(username_from_client), 'r')
 
-                    if password[0] == password_from_client:
+                    # data = login_data.read()
+                    # data = data.split(', ')
+                    # password = data[0]
+
+                    # TODO ask which way you want it to be
+
+                    password = login_data.read().split(', ')[0]
+
+                    if password == password_from_client:
                         client_server_socket.send("success")
                         return True  # Log in success
                     else:
@@ -113,3 +95,57 @@ class Server:
                     client_server_socket.send("no login data found, creating new account".encode())
                     data_from_client = "yes"
 
+    def create_account(self, client_server_socket, username_from_client, password_from_client):
+        # TODO Implement safe passwords with salt (maybe pepper)
+        # TODO Implement public key for RSA Encryption
+        # TODO Check Program privileges
+        while True:
+            # client_server_socket.send("Public key?")
+            # public_key_from_client = client_server_socket.recv(1024).decode()
+
+            """Give Username a random ID and check if ID already exists, if it does, create a new one and repeat"""
+            idArray = []
+            # ID = ID.append(randrange(0, 9))  # TODO ASK IF THIS SHOULD BE A SECURE RANDOM SOURCE
+            for i in range(0, 10000):
+                idArray.append('{:d}'.format(i).zfill(4))  # create array form 0000-9999
+
+            for idArrayLimit in range(9999, -1, -1):
+                index = randrange(0, idArrayLimit)
+                userID = idArray[index]
+                actualUsername = username_from_client + "#" + userID  # example Username: Lmao#0045
+                try:
+                    open('{}.txt'.format(actualUsername), 'r')
+                    del idArray[:index]
+                    continue
+
+                except:
+                    """ Create new file if username+ID doesn't exist 
+                    file consist of: ("Password, Salt, (Message, Sender)*") """
+                    client_server_socket.send(("Your Username is: {}".format(actualUsername)).encode())
+                    login_data = open('{}.txt'.format(actualUsername), "w")
+                    login_data.write(password_from_client)
+                    login_data.close()
+                    return True
+                return True
+            """If Username exists 10000 times already, use another Username"""
+            client_server_socket.send(("Username unavailable. Please select another Username".encode()))
+            username_from_client = client_server_socket.recv(1024).decode()
+
+
+
+    def user_message(self, client_server_socket):
+        client_server_socket.send("go").encode()
+        recipient = client_server_socket.recv(1024).decode()
+        try:
+            # check if file exists; file consist of: ("Password, Salt, (Message, Sender)*")
+            file = open("{name}.txt".format(name=recipient), "r")
+            file.close()
+            user_file = open("{name}.txt".format(name=recipient), "w")
+            client_server_socket.send("send message".encode())
+            message = client_server_socket.recv(1024).decode()
+            user_file.write("")
+            user_file.write("," + message)
+            user_file.close()
+            client_server_socket.send("end message".encode())
+        except:
+            print("excption")
